31421da35eedf9228cfb451ed06e273d3ea5eb12
unknown <sainttrowagmail.com>
2025-05-01 21:52:46 +0800

    Pseudocode

---
+++ quiz_reader.py
@@ -0,0 +1,4 @@
+#Import or make a program that reads the files that "quiz_creator.py" created
+#Ask the user to input what quiz number they want to read
+#Make the user answer the quiz
+#Evaluate the user's answers and give a score

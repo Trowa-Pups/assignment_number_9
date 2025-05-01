9c0b3fbeb557685c15bbd1372021ca4d8630aa10
unknown <sainttrowagmail.com>
2025-05-01 22:00:41 +0800

    I imported os and colorama. I imported os because its the import i used in quiz creator and I imported colorama to satisfy astig factor

--- quiz_reader.py
+++ quiz_reader.py
@@ -1,4 +1,7 @@
 #Import or make a program that reads the files that "quiz_creator.py" created
+import os #Still using os for I am more familiar in it
+import colorama #Importing it because i seen it in my yt feed and i thought i could use it to satisfy the "astig" factor
+
 #Ask the user to input what quiz number they want to read
 #Make the user answer the quiz
 #Evaluate the user's answers and give a score

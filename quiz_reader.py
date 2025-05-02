dbf5abcacb8c6b79f0d0afc48c2b6f3e1d81005a
unknown <sainttrowagmail.com>
2025-05-02 13:25:12 +0800

    Imported the system i made in asking the quiz number and also the system to find the file path aka in the Downloads folder

--- quiz_reader.py
+++ quiz_reader.py
@@ -3,5 +3,12 @@ import os #Still using os for I am more familiar in it
 import colorama #Importing it because i seen it in my yt feed and i thought i could use it to satisfy the "astig" factor
 
 #Ask the user to input what quiz number they want to read
+#Importing this section from my quiz creator
+quiz_number = str(input("Please input what quiz you are going to edit or create Ex.(quiz_#_1.txt): "))
+
+downloads_folder = os.path.join(os.path.expanduser("~"), "Downloads") 
+
+file_path = os.path.join(downloads_folder, quiz_number) 
+
 #Make the user answer the quiz
 #Evaluate the user's answers and give a score

98f3825d08cfc7fbed35586e1b2ba63faec20cb2
unknown <sainttrowagmail.com>
2025-05-03 10:18:48 +0800

    I added time in the program to have a delay in some parts of the quiz to make the flow more natural, and also added \n to some parts to make it look more clean

--- quiz_reader.py
+++ quiz_reader.py
@@ -2,7 +2,8 @@
 import os #Still using os for I am more familiar in it
 import colorama #Importing it because i seen it in my yt feed and i thought i could use it to satisfy the "astig" factor
 import random #Importing it because i need it to randomize the questions 
-from colorama import Fore #Imported Fore to change text color
+from colorama import Fore #Importing Fore to change text color
+import time #Importing it to make a delay in the terminal to make the flow more natural
 
 #Ask the user to input what quiz number they want to read
 #Importing this section from my quiz creator
@@ -15,6 +16,7 @@ file_path = os.path.join(downloads_folder, quiz_number)
 #Make the user answer the quiz
 if os.path.exists(file_path): #To check if the file is there or not
     print(Fore.BLUE + "Reading file...")
+    time.sleep(1)
     with open(file_path, "r") as file: #Opens the files and reads it with "r"
         file_lines = file.readlines()
 
@@ -42,7 +44,8 @@ for line in file_lines:
 quiz_data = list(zip(question_list, right_answer_list)) #Using list(zip()) to correctly pair the question and answer and put it on a lis
 random.shuffle(quiz_data)
 
-print(Fore.GREEN + "🚀  Welcome to " + quiz_number + "! 🚀 \n Answer the quiz by inputting letters like (A, B, C, and D):")
+print(Fore.GREEN + "🚀  Welcome to " + quiz_number + "! 🚀 \n Answer the quiz by inputting letters like (A, B, C, and D):") 
+time.sleep(2) #Made a 2 second delay to make sure the user sees the title
 
 user_score = 0 #The score of the user
 
@@ -54,17 +57,19 @@ for question_number, (question, correct_answer) in enumerate(quiz_data): #Using 
 
 #Evaluate the user's answers and give a score
     if user_answer == correct_answer: #To check
-        print(Fore.GREEN + "You are correct! ✅")
+        print(Fore.GREEN + "\nYou are correct! ✅")
         user_score += 1 #To add one point in the user's score
+        time.sleep(1) #To make it more natural
     
     else:
-        print(Fore.RED + "You are wrong! ❌")
+        print(Fore.RED + "\nYou are wrong! ❌")
+        time.sleep(1) #To make it more natural
 
 if user_score == len(quiz_data): #Using len() to count the amount of things in the list(quiz_data) and seeing if user_score is equal to it
-    print(Fore.YELLOW + "You got a perfect score! 💯 Congratulations!" , user_score , "/" , len(quiz_data))
+    print(Fore.YELLOW + "\nYou got a perfect score! 💯 Congratulations!" , user_score , "/" , len(quiz_data))
 
 elif user_score >= len(quiz_data)// 2: #To see if score is equal or greater than half of the quiz
-    print(Fore.YELLOW + "Nice try! ❤ Better luck next time! 🍀" , user_score , "/" , len(quiz_data))
+    print(Fore.YELLOW + "\nNice try! ❤ Better luck next time! 🍀" , user_score , "/" , len(quiz_data))
 
 else: #If the score is less than half
-    print(Fore.YELLOW + "You tried your best! Keep studying! 📖", user_score , "/" , len(quiz_data))
+    print(Fore.YELLOW + "\nYou tried your best! Keep studying! 📖", user_score , "/" , len(quiz_data))
